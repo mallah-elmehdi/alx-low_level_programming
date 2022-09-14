d51b403e0fd1d80e0e7d63116ab85030c17daf7e
mallah-elmehdi <elmehdimallah@gmail.com>
2022-09-14 09:20:51 -0700

    add header

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,34 +2,6 @@
 #include "main.h"
 
 /**
- * print_sign - function
- * @c: int
- *
- * Return: int
- */
-
-int print_sign(int c)
-{
-	if (c > 0)
-		return (1);
-	else if (c == 0)
-		return (0);
-	return (-1);
-}
-
-/**
- * _abs - function
- * @n: int
- *
- * Return: int
- */
-
-long _abs_long(int n)
-{
-	return (n * print_sign(n));
-}
-
-/**
  * print_last_digit - function
  * @c :int
  * Return: int
@@ -38,8 +10,14 @@ long _abs_long(int n)
 int print_last_digit(int c)
 {
 	int ret;
+	long m;
 
-	ret = _abs_long(c) % 10;
+	if (c >= 0)
+		m = c;
+	else
+		m = -c;
+
+	ret = m % 10;
 	_putchar(48 + ret);
 	return (ret);
 }
