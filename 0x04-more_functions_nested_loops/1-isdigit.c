fd7b34c6b2c2a8c70033040f167b2a2fb01c2ef3
mallah-elmehdi <elmehdimallah@gmail.com>
2022-09-15 16:56:03 -0700

    more

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+/**
+ ** _isdigit - tests function that prints if integer is positive or negative
+ ** @c: param 1
+ **
+ ** Return: 0
+ **/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 66)
+		return (1);
+	return (0);
+}
