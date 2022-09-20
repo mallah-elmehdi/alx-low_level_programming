ffd0fe03ac40969b0060a38a704aae940124a9cd
mallah-elmehdi <elmehdimallah@gmail.com>
2022-09-20 01:05:30 +0100

    pointer

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,24 @@
 #include "main.h"
+
+/**
+ * _strlen - check the code
+ * @s: param
+ *
+ * Return: ...
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s && s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
 /**
  * _puts - check the code
  * @str: param
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,40 @@
 #include "main.h"
+
+/**
+ * _strlen - check the code
+ * @s: param
+ *
+ * Return: ...
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s && s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _puts - check the code
+ * @str: param
+ *
+ * Return: ...
+ */
+
+void _puts(char *str)
+{
+	if (str)
+	{
+		write(1, str, _strlen(str));
+		write(1, "\n", 1);
+	}
+}
+
 /**
  * puts_half - check the code
  * @str: param
