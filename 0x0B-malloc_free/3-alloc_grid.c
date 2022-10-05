7ddb3be8ebef2fc8a5bbd6302d97094863c58f9d
mallah-elmehdi <elmehdimallah@gmail.com>
2022-10-05 04:26:49 +0100

    lib

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,31 +10,30 @@
 
 int **alloc_grid(int width, int height)
 {
-    int **ret;
-    int i;
-    int j;
+	int **ret;
+	int i;
+	int j;
 
-    i = 0;
-    ret = malloc(sizeof(int *) * (height + 1));
-    if (ret == NULL)
-        return (NULL);
-    while (i < height)
-    {
-        ret[i] = malloc(sizeof(int) * width);
-        j = 0;
-        if (ret[i] == NULL)
-        {
-            printf("ok\n");
-            --i;
-            while (i >= 0)
-                free(ret[i--]);
-            free(ret);
-            return (NULL);
-        }
-        while (j < width)
-            ret[i][j++] = 0;
-        i++;
-    }
-    ret[i] = NULL;
-    return (ret);
+	i = 0;
+	ret = malloc(sizeof(int *) * height);
+	if (ret == NULL)
+		return (NULL);
+	while (i < height)
+	{
+		ret[i] = malloc(sizeof(int) * width);
+		j = 0;
+		if (ret[i] == NULL)
+		{
+			printf("ok\n");
+			--i;
+			while (i >= 0)
+				free(ret[i--]);
+			free(ret);
+			return (NULL);
+		}
+		while (j < width)
+			ret[i][j++] = 0;
+		i++;
+	}
+	return (ret);
 }
