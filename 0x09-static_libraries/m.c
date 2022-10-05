7d2791bd4216ceef951613042d6103c207ae06f4
mallah-elmehdi <elmehdimallah@gmail.com>
2022-10-05 01:46:53 +0100

    lib

--- 0x09-static_libraries/m.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-int main(void)
-{
-    printf("%d\n", _abs(1));
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
-}
