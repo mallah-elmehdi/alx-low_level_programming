298f1036857ade6cfe3f56269d62724a0523c6a8
mallah-elmehdi <elmehdimallah@gmail.com>
2022-10-10 18:34:47 +0100

    macros

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,7 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
+#include <limits.h>
 
-#define ABS(x) ((x < 0) ? (-x) : (x))
+#define ABS(x) ((x < 0) ? (x == INT_MAX) ? (INT_MAX + 1) : (-x) : (x))
 
 #endif
