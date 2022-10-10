3af60f1fca6bac39775d02d32207d7578be441a2
mallah-elmehdi <elmehdimallah@gmail.com>
2022-10-10 18:19:53 +0100

    macros

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,20 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-/**
- * _abs - check the code
- * @n: param
- *
- * Return: something
- */
-
-int _abs(int n)
-{
-	if (n < 0)
-		return (-n);
-	return (n);
-}
-
-#define ABS(x) (_abs(x))
+#define ABS(x) ((x < 0) ? (-x) : (x))
 
 #endif
