287d3a0a81a223bf106b8c273db4f131cf69cdf4
mallah-elmehdi <elmehdimallah@gmail.com>
2022-10-10 12:15:19 +0100

    struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,50 @@
 #include "dog.h"
 
 /**
+ * _strlen - check the code
+ * @s: param
+ *
+ * Return: ...
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strdup - see the code
+ * @str: param
+ *
+ * Return: Something.
+ */
+
+char *_strdup(char *str)
+{
+	char *ret;
+	int i;
+
+	i = 0;
+	ret = malloc(_strlen(str) + 1);
+	if (ret == NULL)
+		return (NULL);
+	while (i < _strlen(str))
+	{
+		ret[i] = str[i];
+		i++;
+	}
+	ret[i] = 0;
+	return (ret);
+}
+
+/**
  * new_dog - check the code
  * @name: param
  * @age: param
@@ -16,8 +60,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t) * 2);
 	if (!d)
 		return (NULL);
-	d->name = name;
+	d->name = _strdup(name);
 	d->age = age;
-	d->owner = owner;
+	d->owner = _strdup(owner);
 	return (d);
 }
