c5a3326f0e7e442c4d93a6ff2f8a4348b2f3e5f4
mallah-elmehdi <elmehdimallah@gmail.com>
2022-10-21 13:50:50 +0100

    liked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - check the code
+ * @h: param
+ *
+ * Return: somthing
+ */
+
+size_t print_list(const list_t *h)
+{
+	list_t *n = (list_t *)h;
+	size_t i = 0;
+
+	while (n)
+	{
+		if (n->str)
+			printf("[%u] %s\n", n->len, n->str);
+		else
+			printf("[%u] %s\n", 0, "(nil)");
+		n = n->next;
+		i++;
+	}
+	return (i);
+}
