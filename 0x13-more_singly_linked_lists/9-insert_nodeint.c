93698188455e17e0beb56f217b29f3695003dfee
mallah-elmehdi <elmehdimallah@gmail.com>
2022-10-28 12:17:10 +0100

    fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,6 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp, *new, *prv;
 
 	temp = *head;
+	prv = temp;
 	while (temp && idx)
 	{
 		prv = temp;
