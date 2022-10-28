abeba13c959b9ae0e70f5990472938aa3a147da6
mallah-elmehdi <elmehdimallah@gmail.com>
2022-10-28 12:37:22 +0100

    fix 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,16 +10,22 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp, *new;
+	listint_t *temp, *prv;
 
-	temp = *head;
+	temp = head ? *head : NULL;
+	prv = NULL;
 	while (temp && index)
 	{
+		prv = temp;
 		temp = temp->next;
 		index--;
 	}
 	if (index)
 		return (-1);
+	if (prv)
+		prv->next = temp->next;
+	else if (temp)
+		*head = temp->next;
 	free(temp);
 	return (1);
 }
