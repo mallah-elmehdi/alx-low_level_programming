b656d9f3e4a914cd69cdd956abc649a40be88188
mallah-elmehdi <elmehdimallah@gmail.com>
2022-10-30 23:35:16 +0100

    salam

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,9 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int temp = *n >> index;
 
-	printf("%lu\n", temp);
-
-	if (index > 64)
+	if (index > 32)
 		return (-1);
 	*n -= (temp % 2) << index;
 	return (1);
