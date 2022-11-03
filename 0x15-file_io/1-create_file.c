890ecaafe8607bdddd27ba8cb836c14bb838f134
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-03 03:51:38 +0100

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
-		return (-18);
+		return (-1);
 	if (write(fd, text_content, _strlen(text_content)) < 0)
 		return (-1);
 	return (1);
