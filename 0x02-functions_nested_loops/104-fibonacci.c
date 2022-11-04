0175c7ebccd3f1d026b31799b970e314371d7cb9
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-04 14:27:50 +0100

    lwets

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - function
+ *
+ * Return: som
+ */
+
+int main(void)
+{
+	int i = 0;
+	unsigned long t1 = 0, t2 = 1;
+	unsigned long nextTerm = 0;
+
+	while (i <= 98)
+	{
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+		if (i)
+			printf("%lu", nextTerm);
+		else
+			printf("%d", i);
+		if (i != 98)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
