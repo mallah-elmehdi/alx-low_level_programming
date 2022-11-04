79151ee40d1a108207fd38bc760cc2731c94ecdb
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-04 10:42:58 +0100

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - check the code
+ * @s: param
+ *
+ * Return: ...
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s && s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * read_textfile - check the code
  * @filename: param
  * @letters: param
@@ -35,8 +54,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 			break;
 		buff[r] = 0;
 		len += r;
-		r = write(STDOUT_FILENO, buff, r);
-		if (r < 0)
+		if (write(STDOUT_FILENO, buff, _strlen(buff)) < 0)
 		{
 			free(buff);
 			return (0);
