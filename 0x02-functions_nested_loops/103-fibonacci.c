16312cbb6682cdabdb84ce9ee4a6bb47e0bafb01
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-04 14:22:31 +0100

    lets

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,23 +8,23 @@
 
 int main(void)
 {
-	int is_not_fisrt = 0;
 	long t1 = 0, t2 = 1;
 	long nextTerm = 0;
+	long sum = 0;
 
 	while (1)
 	{
 		t1 = t2;
 		t2 = nextTerm;
 		nextTerm = t1 + t2;
-		if (nextTerm > 4000000)
-			break;
 		if (nextTerm % 2 == 0)
 		{
-			if (is_not_fisrt)
-				printf(", ");
-			printf("%ld", nextTerm);
-			is_not_fisrt = 1;
+			if (nextTerm + sum >= 5000000)
+			{
+				printf("%ld", sum);
+				break;
+			}
+			sum += nextTerm;
 		}
 	}
 	printf("\n");
