f1b04ca1ebd3bb5c0fe747b5772085d05bd0dafc
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-04 11:00:12 +0100

    try

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,14 +9,14 @@
 
 int _strlen(char *s)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (s && s[i])
-    {
-        i++;
-    }
-    return (i);
+	i = 0;
+	while (s && s[i])
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
@@ -29,21 +29,21 @@ int _strlen(char *s)
 
 int copy(int fd_from, int fd_to)
 {
-    char buff[1025];
-    int r;
+	char buff[1025];
+	int r;
 
-    while (1)
-    {
-        r = read(fd_from, buff, 1024);
-        if (r < 0)
-            return (-1);
-        else if (r == 0)
-            break;
-        buff[r] = 0;
-        if (write(fd_to, buff, _strlen(buff)) <= 0)
-            return (0);
-    }
-    return (0);
+	while (1)
+	{
+		r = read(fd_from, buff, 1024);
+		if (r < 0)
+			return (-1);
+		else if (r == 0)
+			break;
+		buff[r] = 0;
+		if (write(fd_to, buff, _strlen(buff)) <= 0)
+			return (-1);
+	}
+	return (0);
 }
 
 /**
@@ -56,40 +56,35 @@ int copy(int fd_from, int fd_to)
 
 int main(int ac, char const *av[])
 {
-    int fd_from;
-    int fd_to;
+	int fd_from;
+	int fd_to;
 
-    if (ac != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
-    fd_from = open(av[1], O_RDONLY);
-    if (fd_from < 0)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-        exit(98);
-    }
-    fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
-    if (fd_to < 0)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-        exit(99);
-    }
-    if (copy(fd_from, fd_to) < 0)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-        exit(98);
-    }
-    if (close(fd_from) < 0)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-        exit(100);
-    }
-    if (close(fd_to) < 0)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-        exit(100);
-    }
-    return (0);
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
+	if (fd_to < 0 || copy(fd_from, fd_to) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	if (close(fd_from) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	return (0);
 }
