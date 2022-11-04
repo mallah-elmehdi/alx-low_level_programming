910c95f198ad19adac286e4ffd0d3d989daf0c09
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-04 11:24:34 +0100

    lets

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * _print - function
+ * @n: int
+ *
+ * Return: void
+ */
+
+void _print(int n, int is_first)
+{
+    if (n >= 100)
+    {
+        if (!is_first)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        putchar((n / 100) + '0');
+        putchar(((n / 10) % 10) + '0');
+        putchar((n % 10) + '0');
+    }
+    else if (n >= 10)
+    {
+        if (!is_first)
+        {
+            putchar(',');
+            putchar(' ');
+            putchar(' ');
+        }
+        putchar((n / 10) + '0');
+        putchar((n % 10) + '0');
+    }
+    else
+    {
+        if (!is_first)
+        {
+            putchar(',');
+            putchar(' ');
+            putchar(' ');
+            putchar(' ');
+        }
+        putchar(n + '0');
+    }
+}
+
+/**
+ * _abs - function
+ * @n: int
+ *
+ * Return: int
+ */
+
+void print_times_table(int n)
+{
+    int i = 0;
+    int j;
+    int is_first;
+
+    if (n > 15 || n < 0)
+        return;
+    while (i <= n)
+    {
+        j = 0;
+        while (j <= n)
+        {
+            is_first = (j == 0) ? 1 : 0;
+            _print(i * j, is_first);
+            j++;
+        }
+        putchar('\n');
+        i++;
+    }
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+#include <stdio.h>
 
 void _putchar(int);
 void print_alphabet(void);
@@ -13,5 +14,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
