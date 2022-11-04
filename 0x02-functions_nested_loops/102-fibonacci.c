ec966b726809fc9ca14e37475894159feae45dd3
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-04 13:07:03 +0100

    s

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,19 +8,20 @@
 
 int main(void)
 {
-	int i = 2;
+	int i = 0;
 	long t1 = 0, t2 = 1;
-	long nextTerm = t1 + t2;
+	long nextTerm = 0;
 
 	while (i < 50)
 	{
 		t1 = t2;
 		t2 = nextTerm;
 		nextTerm = t1 + t2;
-		printf("%ld", nextTerm);
-		i++;
-		if (i != 50)
+		if (i)
+			printf("%ld", nextTerm);
+		if (i + 1 != 50 && i)
 			printf(", ");
+		i++;
 	}
 	printf("\n");
 	return (0);
