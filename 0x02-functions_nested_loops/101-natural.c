03c0836ce5a160479e26fbc4c6b68e4ca9e029d3
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-04 12:00:27 +0100

    salam

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - function
+ *
+ * Return: som
+ */
+
+int main(void)
+{
+    _putchar('2');
+    _putchar('4');
+    _putchar('3');
+    _putchar('2');
+    _putchar('7');
+    _putchar('0');
+    _putchar('\n');
+    return (1);
+}
