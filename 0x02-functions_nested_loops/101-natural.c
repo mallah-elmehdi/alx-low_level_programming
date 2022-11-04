4f9b49206c92392b93d80d26399c2300c7f28d58
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-04 12:13:36 +0100

    zxdf

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,26 @@
 
 int main(void)
 {
-    _putchar('2');
-    _putchar('4');
-    _putchar('3');
-    _putchar('2');
-    _putchar('7');
-    _putchar('0');
-    _putchar('\n');
-    return (1);
+	int i = 0;
+	int sum = 0;
+	int count = 6;
+	char buff[6];
+
+	while (i < 1023)
+	{
+		if (i % 3 == 0)
+			sum += i;
+		else if (i % 5 == 0)
+			sum += i;
+		i++;
+	}
+	while (count > 0)
+	{
+		buff[--count] = (sum % 10) + 48;
+		sum = sum / 10;
+	}
+	while (count < 6)
+		putchar(buff[count++]);
+	putchar('\n');
+	return (1);
 }
