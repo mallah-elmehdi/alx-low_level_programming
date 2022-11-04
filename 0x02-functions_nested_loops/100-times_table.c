7ee2c2620001803f357351bfc33812870ae212ea
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-04 11:25:31 +0100

    ok

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,39 +9,39 @@
 
 void _print(int n, int is_first)
 {
-    if (n >= 100)
-    {
-        if (!is_first)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-        putchar((n / 100) + '0');
-        putchar(((n / 10) % 10) + '0');
-        putchar((n % 10) + '0');
-    }
-    else if (n >= 10)
-    {
-        if (!is_first)
-        {
-            putchar(',');
-            putchar(' ');
-            putchar(' ');
-        }
-        putchar((n / 10) + '0');
-        putchar((n % 10) + '0');
-    }
-    else
-    {
-        if (!is_first)
-        {
-            putchar(',');
-            putchar(' ');
-            putchar(' ');
-            putchar(' ');
-        }
-        putchar(n + '0');
-    }
+	if (n >= 100)
+	{
+		if (!is_first)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n >= 10)
+	{
+		if (!is_first)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		if (!is_first)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar(n + '0');
+	}
 }
 
 /**
@@ -53,22 +53,22 @@ void _print(int n, int is_first)
 
 void print_times_table(int n)
 {
-    int i = 0;
-    int j;
-    int is_first;
+	int i = 0;
+	int j;
+	int is_first;
 
-    if (n > 15 || n < 0)
-        return;
-    while (i <= n)
-    {
-        j = 0;
-        while (j <= n)
-        {
-            is_first = (j == 0) ? 1 : 0;
-            _print(i * j, is_first);
-            j++;
-        }
-        putchar('\n');
-        i++;
-    }
+	if (n > 15 || n < 0)
+		return;
+	while (i <= n)
+	{
+		j = 0;
+		while (j <= n)
+		{
+			is_first = (j == 0) ? 1 : 0;
+			_print(i * j, is_first);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
