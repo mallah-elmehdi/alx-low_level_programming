f67adf044e577064ac1db8346b82145255ae3b7b
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-06 16:24:15 +0100

    asm

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - check the code
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
