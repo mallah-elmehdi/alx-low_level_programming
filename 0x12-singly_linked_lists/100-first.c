bab575b9f5ec1b5de7a27f046b3bf7f1d83bc593
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-06 16:32:55 +0100

    lwta

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,10 @@
  * first - check the code
  */
 
-__attribute__ ((__constructor__)) 
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
+
+void first(void) __attribute__((constructor));
