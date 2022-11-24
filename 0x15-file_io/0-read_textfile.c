1c32d8d55558bd55ec01ae80420d641fac48514a
mallah-elmehdi <elmehdimallah@gmail.com>
2022-11-24 09:32:05 +0100

    read just the length

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -42,23 +42,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buff = malloc(letters + 1);
 	if (buff == NULL)
 		return (0);
-	while (1)
+	r = read(fd, buff, letters);
+	if (r < 0)
 	{
-		r = read(fd, buff, letters);
-		if (r < 0)
-		{
-			free(buff);
-			return (0);
-		}
-		else if (r == 0)
-			break;
-		buff[r] = 0;
-		len += r;
-		if (write(STDOUT_FILENO, buff, _strlen(buff)) < 0)
-		{
-			free(buff);
-			return (0);
-		}
+		free(buff);
+		return (0);
+	}
+	buff[r] = 0;
+	len += r;
+	if (write(STDOUT_FILENO, buff, _strlen(buff)) < 0)
+	{
+		free(buff);
+		return (0);
 	}
 	free(buff);
 	close(fd);
