820e5d601ac997a3ebe67fe812b7bac6b2acb3ca
mallah-elmehdi <elmehdimallah@gmail.com>
2022-12-12 11:08:49 +0100

    style

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistint_len - function that returns the number of elements in a linked dlistint_t list
+ * dlistint_len - function that returns the number of elements
  * @h: dlistint_t list head
  *
  * Return: the number of nodes.
