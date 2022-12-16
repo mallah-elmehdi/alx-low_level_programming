991b0d6a26a675287f4597375410f06b92f99707
mallah-elmehdi <elmehdimallah@gmail.com>
2022-12-16 17:48:44 +0100

    ok

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,50 +1,27 @@
-/**
- * add - addition
- * @a: number
- * @b: nuber
- * Return: result
- */
-int add(int a, int b)
+#include <stdio.h>
+int add(int a,int b)
 {
-      return (a + b);
+	return a + b;
 }
-/**
- * sub - addition
- * @a: number
- * @b: nuber
- * Return: result
- */
-int sub(int a, int b)
+
+int sub(int a,int b)
 {
-      return (a - b);
+        return a - b;
 }
-/**
- * mul - addition
- * @a: number
- * @b: nuber
- * Return: result
- */
-int mul(int a, int b)
+
+int mul(int a,int b)
 {
-      return (a * b);
+        return a * b;
 }
-/**
- * div - addition
- * @a: number
- * @b: nuber
- * Return: result
- */
-int div(int a, int b)
+
+
+int div(int a,int b)
 {
-      return (a / b);
+        return a / b;
 }
-/**
- * mod - addition
- * @a: number
- * @b: nuber
- * Return: result
- */
-int mod(int a, int b)
+
+int mod(int a,int b)
 {
-      return (a % b);
+        return a % b;
 }
+
