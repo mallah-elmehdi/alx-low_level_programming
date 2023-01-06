f048ad1c626c89506bc3d569e299bc269eed6c1a
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-06 21:04:49 +0100

    add null terminator

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@
  * hash_table_create - function that creates a hash table
  *
  * @size: The size of the hash table
- * 
+ *
  * Return: Returns a pointer to the newly created hash table
  * and NULL If something went wrong
  */
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 		return (NULL);
-	new->array = malloc(sizeof(hash_node_t *) * size);
+	new->array = malloc(sizeof(hash_node_t *) * (size + 1));
 	new->size = size;
 	if (new->array == NULL)
 	{
