eca3a29fa3ebe687f7319f0498ab4862af0d33c8
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-06 21:01:18 +0100

    >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table
+ *
+ * @size: The size of the hash table
+ * 
+ * Return: Returns a pointer to the newly created hash table
+ * and NULL If something went wrong
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (new->array == NULL)
+		return (NULL);
+	return (new);
+}
