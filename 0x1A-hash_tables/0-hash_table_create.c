72533687ce58be6b6bb98b7c92a9e433ab599dfd
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-06 21:02:39 +0100

    add size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,6 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new == NULL)
 		return (NULL);
 	new->array = malloc(sizeof(hash_node_t *) * size);
+	new->size = size;
 	if (new->array == NULL)
 		return (NULL);
 	return (new);
