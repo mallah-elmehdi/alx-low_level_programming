31d0970144549f6079b3d5da69baf2ac5ad8c60c
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-06 21:08:58 +0100

    clean memory allocated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,33 @@
 #include "hash_tables.h"
 
 /**
+ * _calloc - check the code
+ * @nmemb: param
+ * @size: param
+ *
+ * Return: somthing.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ret;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ret = malloc(nmemb * size);
+	i = 0;
+	if (ret == NULL)
+		return (NULL);
+	while ((unsigned int)i < nmemb * size)
+	{
+		((unsigned char *)ret)[i] = '\0';
+		i++;
+	}
+	return (ret);
+}
+
+/**
  * hash_table_create - function that creates a hash table
  *
  * @size: The size of the hash table
@@ -12,11 +39,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 
-	new = malloc(sizeof(hash_table_t));
+	new = _calloc(sizeof(hash_table_t), 1);
 	if (new == NULL)
 		return (NULL);
-	if (size)
-		new->array = malloc(sizeof(hash_node_t *) * (size + 1));
+	new->array = _calloc(sizeof(hash_node_t *), size + 1);
 	new->size = size;
 	if (new->array == NULL)
 	{
