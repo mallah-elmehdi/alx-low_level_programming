6aa483e8ccd5f0eaa6cdc08c3582965a87d044f8
mallah-elmehdi <elmehdimallah@gmail.com>
2023-01-06 21:03:39 +0100

    free the hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,6 +18,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new->array = malloc(sizeof(hash_node_t *) * size);
 	new->size = size;
 	if (new->array == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	return (new);
 }
